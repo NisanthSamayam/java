33ae6361b6ea1e6427b8dc43e77802331f39b73b
Nisanth <151718208+NisanthSamayam@users.noreply.github.com>
2024-09-05 11:09:13 +0530

    Add files via upload

---
+++ student.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.Scanner;
+class Elseif
+{
+	public static void main(String args[])
+	int sm1,sm2,sm3,sm4,sm5;
+	double avg;
+	Scanner sc =new Scanner (System.in);
+	System.out.println("Enter marks of 5 subjects: ");
+	sm1 = out.nextln();
+	sm2 = sc.nextln();
+	sm3 = sc.nextln();
+	sm4 = sc.nextln();
+	sm5 = sc.nextln();
+	avg=(double)(sum1+sum2+sum3+sum4+sum5)/5;
+	{
+	System.out.println("Student avg is "+avg );
+	if(avg>=80)
+	{
+		System.out.println("Student passed in Distinction");
+	}
+	else if (avg >= 70 && avg < 80)
+	{
+		System.out.println("Student passed in first class");
+	}
+	else if(avg >=50 && avg < 60)
+	{
+		System.out.println("Student passed in second class");
+	}
+	else if(avg >=40 && < 50)
+	{
+		System.out.println("Student passed in third class");
+	}
+	else
+	{
+		System.out.println("Student is failed");
+	}
+  }
+ }
+}		`
+
+	
